1a7ac1ec3d1c4151c59dcc7f07242bc4892a5de8
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2017-11-08 09:02:47 -0200

    Atualização SQL - Consultas c/ Select

--- select_93.sql
+++ select_93.sql
@@ -4,3 +4,5 @@ select * from estadocivil where estadocivil.nome = 'SOLTEIRO' or estadocivil.nom
 select * from endereco where fk_bairro_id >= 1  and fk_logradouro_id <= 2;
 select nome as "Pessoas que moram no endereço 10: " from pessoa where fk_endereco_id =10;
 select nome as "Cidades do ES:" from cidade where fk_estado_id = 1;
+select nome, dataocorrencia, age(dataocorrencia) as "Quanto tempo se passou", age(dataocorrencia) - age(dataregistro) as "Tempo até registrar" from ocorrencia inner join pessoa on (ocorrencia.fk_pessoafisica_fk_pessoa_id=pessoa.id);
+select date_part('year',MAX(dataocorrencia)) - date_part('year',MIN(dataocorrencia)) as "Diferença da data mais recente e da mais antiga (Em anos)" from ocorrencia;
