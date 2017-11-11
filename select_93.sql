005edd3c80910f97c299cfed80af9b6eb67bdcca
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2017-11-11 17:36:53 -0200

    UPDATE SQL
    Inserção do SQL atual

--- select_93.sql
+++ select_93.sql
@@ -1,8 +1,11 @@
-select nome from pessoa where fk_endereco_id = 9 or fk_endereco_id = 11;
-select nome from endereco where fk_bairro_id >=5;
-select * from estadocivil where estadocivil.nome = 'SOLTEIRO' or estadocivil.nome = 'VIUVO';
+/*Select Lógico*/
 select * from endereco where fk_bairro_id >= 1  and fk_logradouro_id <= 2;
+select nome from pessoa where fk_endereco_id = 9 or fk_endereco_id = 11;
+
+/*Select As*/
 select nome as "Pessoas que moram no endereço 10: " from pessoa where fk_endereco_id =10;
 select nome as "Cidades do ES:" from cidade where fk_estado_id = 1;
+
+/*Select Arithmetic*/
 select nome, dataocorrencia, age(dataocorrencia) as "Quanto tempo se passou", age(dataocorrencia) - age(dataregistro) as "Tempo até registrar" from ocorrencia inner join pessoa on (ocorrencia.fk_pessoafisica_fk_pessoa_id=pessoa.id);
 select date_part('year',MAX(dataocorrencia)) - date_part('year',MIN(dataocorrencia)) as "Diferença da data mais recente e da mais antiga (Em anos)" from ocorrencia;
--- select_like.sql
+++ select_like.sql
@@ -1,4 +1,4 @@
 select nome from pessoa where nome LIKE 'C%';
 select nome from pessoa where nome LIKE '%VITÓRIA';
-select nome from pessoa where nome LIKE '%SILVA%';
+select nome from pessoa where nome LIKE '%LIVIA%';
 select nome from pessoa where nome NOT LIKE 'C%';
