79077e941af272d8e0191e2a96a37c2f33780b15
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2017-12-04 11:48:22 -0200

    left_right_Join

---
+++ left_right_join.sql
@@ -0,0 +1,7 @@
+/* RIGHT JOIN */
+select cidade.nome, estado.nome from cidade right outer join estado on (cidade.fk_estado_id = estado.id)
+select cpf, rg, datanascimento, cidade.nome from pessoafisica right outer join cidade on (pessoafisica.fk_cidade_id = cidade.id)
+
+/* LEFT JOIN */
+select nome, login, senha from pessoa left outer join usuario on(pessoa.id = usuario.id)
+select nacionalidade.nome, count(*) from nacionalidade left outer join pessoafisica on(nacionalidade.id = pessoafisica.fk_nacionalidade_id) group by nacionalidade.nome
